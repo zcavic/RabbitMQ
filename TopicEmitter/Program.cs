cbe600c0491ecc23ccd5ce4889bbaae4f0feb0ff
Zeljko Cavic <59483556+zcavic@users.noreply.github.com>
2023-01-22 19:21:32 +0100

    Add files via upload

---
+++ TopicEmitter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+class EmitLogTopic
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "yomingo.models.events.clientdeletedevent",
+                                    type: "topic");
+
+            var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+            
+            var message = new Message() { ClientId = 1 };
+
+            var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+            channel.BasicPublish(exchange: "yomingo.models.events.clientdeletedevent",
+                                 routingKey: routingKey,
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+        }
+    }
+}
+
+
+    public class Message
+{
+    public int ClientId { get; set; }
+}
---
+++ TopicReceiver/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class ReceiveLogsTopic
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "yomingo.models.events.clientdeletedevent", type: "topic");
+            var queueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(queue: queueName,
+                              exchange: "yomingo.models.events.clientdeletedevent",
+                              routingKey: "#");
+
+            Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var routingKey = ea.RoutingKey;
+                Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                  routingKey,
+                                  message);
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
